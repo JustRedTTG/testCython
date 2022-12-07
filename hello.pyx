af2f00a4dde0d3c150983498dfaad0f7396e6992
RedTTG <redtonehair@gmail.com>
2022-12-07 15:40:34 +0200

    using pyximport and test time woaaahh
    you need the cython package to run this :smiley: :+1:

--- hello.pyx
+++ hello.pyx
@@ -1,3 +1,4 @@
+import time
 from libc.math cimport pow
 
 cdef double square_and_add (double x):
@@ -8,6 +9,14 @@ cdef double square_and_add (double x):
     """
     return pow(x, 2.0) + x
 
+cdef void test_speed():
+    s: int = 0
+    for i in range(100_000_000):
+        s += i
+
 cpdef print_result (double x):
     """This is a cpdef function that can be called from Python."""
     print("({} ^ 2) + {} = {}".format(x, x, square_and_add(x)))
+    before = time.time()
+    test_speed()
+    print(f'test speed cython: {time.time()-before:.02f}s')
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # Import the extension module hello.
+import pyximport
+import time
+pyximport.install()
 import hello
 
+
 # Call the print_result method
+print('It will test the speed of summing 100_000_000, so umm, wait a bit\nMy testing indicated a huge difference')
 hello.print_result(23.0)
+before = time.time()
+s: int = 0
+for i in range(100_000_000):
+    s += i
+print(f'test speed python: {time.time()-before:.02f}s')
